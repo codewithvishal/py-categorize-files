00731a83e34c0ef0be2dd452493513986bb0d2cc
Vishal Chopra <34140031+digitalvishal19@users.noreply.github.com>
2019-07-28 09:43:45 +0530

    Update categorize.py

--- categorize.py
+++ categorize.py
@@ -15,8 +15,8 @@ class Categorize():
         total_file_moved = 0
 
         # to check if images, songs, videos, pdf dir is there if not then create a new one
-        if not os.path.exists(cwd+"\\"+self.movedir):
-            os.makedirs(cwd+"\\"+self.movedir)
+        if not os.path.exists(os.path.join(cwd, movedir)):
+            os.makedirs(os.path.join(cwd, movedir))
         
         # Now Move the file to their respctive directory
         for item in dir_files:
@@ -24,7 +24,7 @@ class Categorize():
             if os.path.isfile(item):
                 if item.endswith(self.extension):
                     total_file_moved += 1
-                    shutil.move(item, self.path+"\\"+self.movedir)
+                    shutil.move(item, os.path.join(cwd, movedir))
         
         print({
             "Status": "Completed",
